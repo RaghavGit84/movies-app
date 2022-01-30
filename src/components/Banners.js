5d96e8546989d1c5927d6bbf33053dba6e041ff2
Raghav Gupta <raghav.gupta8435@gmail.com>
2022-01-30 23:49:51 +0530

    update

--- src/components/Banners.js
+++ src/components/Banners.js
@@ -1,18 +1,31 @@
-import {movies} from './getMovies';
+import { movies } from './getMovies';
 
 import React, { Component } from 'react';
 
 export default class Banners extends Component {
   render() {
+    // console.log(movies)
+    let movie = movies.results[0]
+    
     return (
-        <div className="card">
-            <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
+      <>
+        {
+
+          movie == "" ? <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div> :
+
+            <div className="card banner-card">
+              <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}   alt={movie.title} className="card-img-top banner-img"/>
+              <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
             </div>
-            </div>
+        }
+
+      </>
     )
   }
 }

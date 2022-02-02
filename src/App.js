d52c7250de6283062ecdf7f02f8f35ed0bf07542
Raghav Gupta <raghav.gupta8435@gmail.com>
2022-02-03 03:58:00 +0530

    updated design of 1st pg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import Navbar from './components/Navbar';
 import Banners from './components/Banners';
+import Movies from './components/Movies';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar/>
       <Banners/>
+      <Movies/>
     </>
   );
 }

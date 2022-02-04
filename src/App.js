4cec6f1114caa5683aa03990c71e66cac16c8a58
Raghav Gupta <raghav.gupta8435@gmail.com>
2022-02-05 04:39:14 +0530

    updated fav pg

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import Navbar from './components/Navbar';
 import Banners from './components/Banners';
 import Movies from './components/Movies';
+import Favourite from './components/Favourite';
 import './App.css';
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Banners/>
-      <Movies/>
+      {/* <Banners/>
+      <Movies/> */}
+      <Favourite/>
     </>
   );
 }
